a8cabd907e84b6eead1071f4b9fd34de38261b34
igormath <igorxt9@gmail.com>
2025-02-15 18:15:26 -0300

    update main.py w/ obrigacao_acessoria routes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from router_empresa import router_empresa
+from router_obrigacao_acessoria import router_obrigacao_acessoria
 
 app = FastAPI()
 
 app.include_router(router=router_empresa)
+app.include_router(router=router_obrigacao_acessoria)
